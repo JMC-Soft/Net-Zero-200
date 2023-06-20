bcada20398c1f0a5133f74cba16a8d6d0144348f
Lycoris62 <wodbs7758@naver.com>
2023-06-20 18:11:51 +0900

    feat: backend 첫 커밋

---
+++ back/vegreen/src/test/java/com/jmcsoft/vegreen/VegreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmcsoft.vegreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
